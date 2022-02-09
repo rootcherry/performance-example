f119e3c557a1bcadafef756736316158bd1f9371
Chris N <hi.chrisn@gmail.com>
2022-02-09 17:37:51 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cluster = require('cluster');
+const os = require('os');
+
+const app = express();
+
+function delay(duration) {
+  const startTime = Date.now();
+  while (Date.now() - startTime < duration) {
+    // event loop is blocked...
+  }
+}
+
+app.get('/', (req, res) => {
+  // JSON.stringify({}) => "{}"
+  // JSON.parse("{}") => {}
+  // [5,1,2,3,4].sort()
+  res.send(`Performance example: ${process.pid}`);
+});
+
+app.get('/timer', (req, res) => {
+  // delay the response
+  delay(4000);
+  res.send(`Beep beep beep! ${process.pid}`);
+});
+
+console.log('Running server.js...');
+
+if (cluster.isMaster) {
+  console.log('Master has been started...');
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log('Worker process started.');
+  app.listen(3000);
+}
